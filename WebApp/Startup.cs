8502f0ef423007601d4882a539325e063649c40e
Tushar Khachane <61828288+tushar8871@users.noreply.github.com>
2021-07-21 09:48:58 +0530

    Update Startup.cs

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using  Microsoft.VisualStudio.Web.BrowserLink;
 using WebApp.Models;
 
 namespace WebApp
